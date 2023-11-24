248754322fe8ed83ad63c26951a9fab0e629cf05
ozgebahceci <ozge.bahceci@hotmail.com>
2023-11-24 17:07:19 +0300

    Add project files.

---
+++ MeetingApp/Controllers/MeetingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+    public class MeetingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Apply()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
