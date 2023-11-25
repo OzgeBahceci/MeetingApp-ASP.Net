4a2c292cef39c83c07d4ff8c1ea03c020dbd0efc
OzgeBahceci <ozge.bahceci@hotmail.com>
2023-11-25 14:55:23 +0300

    Model ile string veri taşıma (saate göre karşılama mesajı)

--- MeetingApp/Controllers/HomeController.cs
+++ MeetingApp/Controllers/HomeController.cs
@@ -6,27 +6,15 @@ namespace MeetingApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+       
 
         public IActionResult Index()
         {
-            return View();
+            int saat = DateTime.Now.Hour;
+            var selamla = saat>12 ? "İyi günler":"Günaydın";
+            return View(model:selamla);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
